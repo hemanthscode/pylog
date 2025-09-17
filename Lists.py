b4438f740ccb0788277cf64bd1f4133bd24e0fed
hemanthscode <hemanths7.dev@gmail.com>
2025-09-17 21:18:42 +0530

    Day 1: Hello World

---
+++ Lists.py
@@ -0,0 +1,28 @@
+cars = ["BMW", "Volvo", "Ford"]
+cars.insert(1, "Toyota") 
+cars.append("Honda")
+
+# for x in cars:
+#     print(x)
+
+# i = 0
+# while i < len(cars):
+#     print(cars[i])
+#     i += 1
+
+# for i in range(len(cars)):
+#     print(cars[i])
+
+# [print(x) for x in cars]
+
+# newList = [x for x in cars if x != "Ford"]
+# print(newList)
+
+# newList = [x.upper() for x in cars]
+# print(newList)
+
+# cars.sort()
+# print(cars)
+newList = []
+for x in cars:
+    print(x)
